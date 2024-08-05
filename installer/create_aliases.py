914c8be943470a10258197ecf3c94b80b8d2b02a
Kheeran D. <kheerand@gmail.com>
2024-08-05 19:44:52 +1000

    fixed incorrect aliases template

--- installer/create_aliases.py
+++ installer/create_aliases.py
@@ -4,14 +4,14 @@ import os, sys
 alias_filename = './fabric_patterns_aliases'
 patterns = os.listdir('../patterns')
 
-fabric_prefix = "fabric --pattern {}\n"
-fabric_copy_prefix = "fabric --copy --pattern {}\n"
+fabric_prefix = "alias {}='fabric --pattern {}'\n"
+fabric_copy_prefix = "alias c_{}='fabric --copy --pattern {}'\n"
 
 f=open(alias_filename,"w")
 
 for pattern in patterns:
-	alias1 = fabric_prefix.format(pattern)
-	alias2 = fabric_copy_prefix.format(pattern)
+	alias1 = fabric_prefix.format(pattern,pattern)
+	alias2 = fabric_copy_prefix.format(pattern,pattern)
 
 	f.writelines(alias1)
 	f.writelines(alias2)
